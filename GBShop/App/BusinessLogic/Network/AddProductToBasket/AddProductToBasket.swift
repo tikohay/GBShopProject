58a8ea8f34a2bb42dc8e63ff83be1b921c1962b4
Levon Karakhanyan <levon98@bk.ru>
2021-09-22 17:12:04 +0300

    fixed base url

--- GBShop/App/BusinessLogic/Network/AddProductToBasket/AddProductToBasket.swift
+++ GBShop/App/BusinessLogic/Network/AddProductToBasket/AddProductToBasket.swift
@@ -11,7 +11,7 @@ class AddProductToBasket: AbstractRequestFactory {
     let errorParser: AbstractErrorParser
     let sessionManager: Session
     let queue: DispatchQueue
-    let baseUrl = URL(string: "http://127.0.0.1:8080")!
+    let baseUrl = URL(string: "https://salty-bastion-35523.herokuapp.com")!
     
     init(
         errorParser: AbstractErrorParser,
